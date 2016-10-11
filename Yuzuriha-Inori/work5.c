e10a14b6987c801f3ffb1291492291f645da53bf
yixi qi <qiyixi2010@Gmail.com>
2016-10-11 18:01:38 +0900

    1010work

---
+++ Yuzuriha-Inori/work5.c
@@ -0,0 +1,77 @@
+//#include<stdio.h>
+//#include<string.h>
+//int main()
+//{	char arr1[]={"Inori is my love"};
+//	char arr2[]={"****************"};
+//	int left = 0;
+//	int right = strlen(arr1)-1;
+//	while(left<=right)
+//	{	
+//		arr2[left] = arr1[left];
+//		arr2[right] = arr1[right];
+//		left++;right--;
+//		printf("%s\n",arr2);
+//	}
+//return 0;
+//}
+//#include<stdio.h>
+//int main()
+//{
+//	int pass = 123456;
+//	int enter;
+//	int i;
+//	printf("请输入密码：");
+//for(i=0;i<4;i++)
+//{	scanf("%d",&enter);
+//  if(pass==enter)
+//  {printf("密码正确");
+//	  break;}
+//  else printf("请重新输入");
+//	  continue;
+//}
+//return 0;
+//}
+//#include<stdio.h>
+//int main()
+//{
+//	int a,i;
+//	int b;
+//	printf("please enter a number:");
+//	scanf("%d",&a);
+//	while(a!=0){
+//		b=a%10;
+//printf("%d\n",b);
+//a=a/10;}
+//	return 0;
+//}
+
+//#include<stdio.h>
+//int main()
+//{
+//	float sum=0.0;
+//	float a=1;
+//	int i;
+//	for (i=1;i<101;i++)
+//{
+//	sum=sum+a/i;
+//	a=a*(-1);
+//}
+//	printf("%f",sum);
+//	return 0;
+////}
+//#include<stdio.h>
+//int main()
+//{
+//	int n;
+//	int a =0;
+//	int count = 0;
+//	for(a=1;a<101;a++){
+//		n=a;
+//	while(n){
+//	if(n%10==9){
+//		count++;}
+//	n=n/10;
+//	}}
+//	printf("0到100中有%d个9",count);
+//	return 0;
+//}
