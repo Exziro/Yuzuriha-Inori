91cab94f230a396ecc7d733a94f30f4be39804e3
yixi qi <qiyixi2010@Gmail.com>
2016-10-26 00:05:05 +0900

    compose two number!

---
+++ Yuzuriha-Inori/20161025.c
@@ -0,0 +1,30 @@
+//#include<stdio.h>
+//int main()
+//{
+//	int a;
+//	int arr[32];
+//	int brr[32];
+//	int b;
+//	int i=0;
+//	int count=0;
+//	printf("please enter two number:");
+//	scanf("%d %d",&a,&b);
+//	for(i=0;a!=0;i++)
+//	{
+//	arr[i]=a%2;
+//		a=a/2;
+//	}
+//	for(i=0;b!=0;i++)
+//	{   
+//		brr[i]=b%2;
+//		b=b/2;
+//	}
+//	for(i=0;i<32;i++)
+//	{
+//		if(arr[i]!=brr[i])
+//			count++;
+//	}
+//	printf("they have %d diffrence.\n",count);
+//	return 0;
+//}
+
