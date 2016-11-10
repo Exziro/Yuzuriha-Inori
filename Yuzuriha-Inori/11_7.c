c661aa71d4093bd41d92f5e02cb784cc54c92d1b
yixi qi <qiyixi2010@Gmail.com>
2016-11-10 21:38:23 +0900

    check the single number

--- Yuzuriha-Inori/11_7.c
+++ Yuzuriha-Inori/11_7.c
@@ -105,41 +105,97 @@
 //}
 
 
-int Strstr(char*arr,char*string)
-{
-	int i,j,k;
-	int count=0;
-	int counts=0;
-	int len=strlen(arr);
-	while(1)
-	{
-		while(*arr!='0')
-		{if(*arr++==*string)
-			{*string++;
-			counts++;}
-		if(*string=='\0')
-			break;
-		if(*arr=='\0')
-		{
-			count++;
-			arr=arr-len;
-		}
-		}
-		if(count==1)
-			break;
-	}
-	if(counts==6)
-		return 1;
-	else return 0;
-}
+//int Strstr(char*arr,char*string)
+//{
+//	int i,j,k;
+//	int count=0;
+//	int counts=0;
+//	int len=strlen(arr);
+//	while(1)
+//	{
+//		while(*arr!='0')
+//		{if(*arr++==*string)
+//			{*string++;
+//			counts++;}
+//		if(*string=='\0')
+//			break;
+//		if(*arr=='\0')
+//		{
+//			count++;
+//			arr=arr-len;
+//		}
+//		}
+//		if(count==1)
+//			break;
+//	}
+//	if(counts==6)
+//		return 1;
+//	else return 0;
+//}
+//int main()
+//{
+//	int i;
+//	char a[]="abcdef";
+//	char b[]="cdefab";
+//	i=Strstr(a,b);
+//	printf("%d",i);
+//	system("pause");
+//	return 0;
+//}
+//
+//int main()
+//{
+//	int a=1;
+//	int b=1;
+//	int s=0;
+//	int i=0;
+//	int n=0;
+//	printf("Wich number do you need?\n");
+//	scanf("%d",&n);
+//	for(i=1;i<n;i++)
+//	{
+//		s=a+b;
+//		b=a;
+//		a=s;
+//	}
+//	printf("the %d number is %d",n,b);
+//	system("pause");
+//	return 0;
+//}
+//void Find(int *arr)
+//{
+//	;
+//	int i=0;
+//	for(i=0;i<len;i++)
+//	{	if(arr[i]<arr[0]);
+//			arr[0]=arr[i];
+//	}
+//}
 int main()
 {
-	int i;
-	char a[]="abcdef";
-	char b[]="cdefab";
-	i=Strstr(a,b);
-	printf("%d",i);
+	int a[]={4,4,7,8,5,6,5,6};
+	int i =0;
+	int j =0;
+	int b=0;
+	for(i=0;i<sizeof(a)/sizeof(a[0])-1;i++)
+		for(j=0;j<sizeof(a)/sizeof(a[0])-1-i;j++)
+		{
+			if(a[j+1]<a[j])
+				{b=a[j];
+				a[j]=a[j+1];
+				a[j+1]=b;}
+		}
+	for(i=0;i<sizeof(a)/sizeof(a[0]);i+=2)
+	{
+		if (a[i]!=a[i+1])
+		{
+			printf("%d\n",a[i]);
+			i--;
+		}
+	//for(i=0;i<7;i++)
+	//	printf("%d",a[i]);
+	}
 	system("pause");
 	return 0;
-}
 
+}
