160ef40020c648846953aba494b9f8e008d5f5bf
yixi qi <qiyixi2010@Gmail.com>
2016-11-13 19:14:56 +0900

    printf

---
+++ Yuzuriha-Inori/11_13.c
@@ -0,0 +1,43 @@
+#include<stdio.h>
+#include<windows.h>
+#include<stdarg.h>
+void myprintf(char*fmt,...)
+{
+	va_list pArg;
+	char c;
+	va_start(pArg,fmt);
+	do
+	{
+		c=*fmt;
+		if(c!='%')
+		{
+			putchar(c);
+		}
+		else
+		{
+			switch(*++fmt)
+			{
+			case 'd':
+				printf("%d",*((int*)pArg));
+				break;
+			case 'f':
+				printf("%d",*((float*)pArg));
+				break;
+			default:
+				break;
+			}
+			va_arg(pArg,int);
+		}
+		++fmt;
+	}while(*fmt!='\0');
+	va_end(pArg);
+	return;
+}
+int main()
+{	int i=1234;
+	int j=5678;
+	myprintf("test one:=%d",i);
+	myprintf("test one:=%f",i);
+	system("pause");
+	return 0;
+}
