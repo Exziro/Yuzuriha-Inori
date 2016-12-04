837cac5a5095951dc55cab166bb488a5114ce8bb
yixi qi <qiyixi2010@Gmail.com>
2016-12-04 22:06:23 +0900

    address book

---
+++ Yuzuriha-Inori/12_4.c
@@ -0,0 +1,17 @@
+#include<stdio.h>
+#include"addressbook.h"
+void menue()
+{
+	printf("*****AddressBook****\n");
+	printf("********************\n");
+	printf("***1.add****2.Find**\n");
+	printf("***3.change*4.show**\n");
+	printf("***5.empty**6.sort**\n");
+	printf("*******0.excit******\n");
+}
+void Add();
+void Find();
+void Show();
+void Empty();
+void Sort();
+void Change();
