e588259da941c800b3d7f99bc9fa5e459aaa6e57
yixi qi <qiyixi2010@Gmail.com>
2016-12-11 23:11:15 +0800

    char change int

--- Yuzuriha-Inori/12_10.c
+++ Yuzuriha-Inori/12_10.c
@@ -1,21 +1,21 @@
 #include<stdio.h>
 #include<windows.h>
-int Change(char*p)
-{
-	int sum =0;
-	while(*p!='\0')
-	{
-		sum=10*sum+*p-'0';
-		p++;
-	}
-	return sum;
-}
-int main()
-{
-	char a[]="1234";
-	int p = 0;
-	p=Change(a);
-	printf("%d\n",p);
-	system("pause");
-	return 0;
-}
+//int Change(char*p)
+//{
+//	int sum =0;
+//	while(*p!='\0')
+//	{
+//		sum=10*sum+*p-'0';
+//		p++;
+//	}
+//	return sum;
+//}
+//int main()
+//{
+//	char a[]="1234";
+//	int p = 0;
+//	p=Change(a);
+//	printf("%d\n",p);
+//	system("pause");
+//	return 0;
+//}
