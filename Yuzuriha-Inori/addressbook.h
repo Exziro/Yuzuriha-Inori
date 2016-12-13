6b295361e8edc5290273fbb2fcf1723e962b1c1a
yixi qi <qiyixi2010@Gmail.com>
2016-12-13 23:27:23 +0900

    set the addressbook.h

--- Yuzuriha-Inori/addressbook.h
+++ Yuzuriha-Inori/addressbook.h
@@ -1,5 +1,20 @@
 #include<stdio.h>
 #include<windows.h>
+typedef struct person
+{
+	char name[256];
+	char sex;
+	unsigned char age;
+	char phone[16];
+	char addre[256];
+}person_t,*person_p,**person_pp;
+typedef struct contact 
+{
+	int cap;
+	int size;
+	person_p contactlist;
+}contact_t,*contact_p,**contact_pp;
+
 void menue();
 void Add();
 void Find();
