799a713f99c0d6e738eedbfbd958a28de369f20d
yixi qi <qiyixi2010@Gmail.com>
2016-12-17 22:04:58 +0900

    set the addressbook.c

---
+++ Yuzuriha-Inori/addteset.c
@@ -0,0 +1,33 @@
+#include<stdio.h>
+#include<windows.h>
+#include"addressbook.h"
+#define _size 1000
+int main()
+{
+	
+	//int n;
+	//while(1)
+	//{
+	//	menue();
+	//	printf("please change:");
+	//	scanf("%d",&n);
+	//	switch (n)
+	//	{
+	//	case 1:Add();break;
+	//	case 2:Find();break;
+	//	case 3:Change();break;
+	//	case 4:Show();break;
+	//	case 5:Empty();break;
+	//	case 6:Sort();break;
+	//	case 0:exit;
+	//	default: printf("erro:please try again");
+	//		break;
+	//	}
+	contact_p mycontact=NULL;
+	initcontact(&mycontact)
+
+
+		system("pause");
+		return 0;
+	}
+}
