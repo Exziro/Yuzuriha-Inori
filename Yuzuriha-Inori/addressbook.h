88e9d1d94be51470e6eccba9d5d42b99a30430bf
yixi qi <qiyixi2010@Gmail.com>
2016-12-25 22:36:46 +0800

    datebool

--- Yuzuriha-Inori/addressbook.h
+++ Yuzuriha-Inori/addressbook.h
@@ -21,8 +21,8 @@ static int isful(contact_p p);//判断创建的内容是否已满
 static int  aadcontact(contact_p p);//自增一个新的存储空间
 void Destorycontact(contact_p p);//清空
 int Add(contact_p p1,person_t p2 );//添加通讯录
-void Find();
+void Find(contact_p p);
 void Show(contact_p p);
-void Empty();
+void Empty(contact_p p);
 void Sort();
 void Change();
