3f2a53b1066e3f0abb22f55f255610167b693536
yixi qi <qiyixi2010@Gmail.com>
2017-01-08 22:11:33 +0800

    dateline

--- Yuzuriha-Inori/dateline.c
+++ Yuzuriha-Inori/dateline.c
@@ -3,18 +3,18 @@
 #include"test.h"
 typedef int DateType;
 #define Maxsize 10;
-//typedef struct SeqList 
-//{
-//	DateType arr[Maxsize];
-//	int size;
-//}Seqlist;
-//void InitSqlist(Seqlist * seq)
-//{
-//	seq->size=0;
-//}
-//void insert(Seqlist * seq)
-//{
-//
-//
-//61BEI;69BEI;75BEI;82BEI;103BEI;113BEI
+typedef struct SeqList 
+{
+	DateType arr[Maxsize];
+	int size;
+}Seqlist;
+void InitSqlist(Seqlist * seq)
+{
+	seq->size=0;
+}
+void insert(Seqlist * seq)
+{
+
+
+61BEI;69BEI;75BEI;82BEI;103BEI;113BEI
 
