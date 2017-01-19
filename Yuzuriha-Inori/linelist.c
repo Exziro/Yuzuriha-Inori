675be4e40a44dcda1a2f50f1b1bd579f54768768
yixi qi <qiyixi2010@Gmail.com>
2017-01-19 08:50:25 +0800

    linelist init!

--- Yuzuriha-Inori/linelist.c
+++ Yuzuriha-Inori/linelist.c
@@ -1,5 +1,6 @@
 #include<stdio.h>
 #include<malloc.h>
+#include<assert.h>
 typedef int Datetype;
 typedef struct Node
 {
@@ -12,4 +13,57 @@ void InitList(Pnode* phead)
 	if(phead==NULL)
 		printf("init erro");
 	(*phead)->next=NULL;
+}
+void Pushback(Pnode* phead,Datetype data)//Î²²å
+{
+	Pnode  _new=(Node*)malloc(sizeof(struct Node));
+	//Node * i=(*phead);
+	if(_new==NULL)
+	{
+		printf("wrong!!");
+	}
+	assert(phead);
+	_new->date=data;
+	if((*phead)->next==NULL)
+	{
+		(*phead)->next=_new;
+		_new->next=NULL;
+	}
+	else
+		while((*phead)->next)
+		{
+			(*phead)=(*phead)->next;
+		}
+		(*phead)->next=_new;
+}
+void Pusherase(Pnode* phead)//É¾³ýÎ²Á´±í
+{
+	//Pnode  _new=(Node*)malloc(sizeof(struct Node));
+	assert(phead);
+	if((*phead)->next==NULL)
+	{
+		*phead=NULL;
+	}
+			while((*phead)->next)
+		{
+			(*phead)=(*phead)->next;
+		}
+		(*phead)=NULL;
+}
+void PushFront(Pnode * phead,Datetype date)
+{
+	Pnode  _new=(Node*)malloc(sizeof(struct Node));
+		if(_new==NULL)
+	{
+		printf("wrong!!");
+	}
+	_new->date=date;
+	if((*phead==NULL)){
+		(*phead)->date=date;
+		(*phead)->next=NULL;
+	}
+	else{
+		_new->next=(*phead)->next;
+		(*phead)->next=_new;
+	}
 }
