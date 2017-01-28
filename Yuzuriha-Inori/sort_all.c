ed884bb41dffcccde980b305739f6ca06abea2af
yixi qi <qiyixi2010@Gmail.com>
2017-01-28 18:18:32 +0800

    three easy sort

--- Yuzuriha-Inori/sort_all.c
+++ Yuzuriha-Inori/sort_all.c
@@ -1,4 +1,5 @@
 #include<stdio.h>
+#include<string.h>
 int sequence(int s[],int n,int key)//顺序查找改良型
 {
 	int i;
@@ -10,5 +11,25 @@ int sequence(int s[],int n,int key)//顺序查找改良型
 			return -1;
 	}
 }//将需要查找的目的数据放在数组的最后一个 省去比较步骤
-int midsort(int s[])
+int midsort(int s[],int key)
 {
+	int E=strlen(s);
+	int F =0;
+	int mid;
+	if(key==s[E])
+		return E;
+	if(key==s[F])
+		return F;
+	while(F<=E)
+	{
+		mid=(E+F)/2;
+		if(s[mid]==key)
+			return mid;
+		if(key<s[mid])
+		{
+			E=mid-1;
+		}
+		else
+			F=mid+1;
+	}
+}
