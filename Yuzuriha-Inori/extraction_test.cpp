205df64c0a8bd150122347d8bebd936cb0b38507
yixi qi <qiyixi2010@Gmail.com>
2017-03-18 20:33:38 +0800

    test extraction

---
+++ Yuzuriha-Inori/extraction_test.cpp
@@ -0,0 +1,63 @@
+#include<iostream>
+using namespace std;
+struct _TrueType
+{
+	bool Get()
+	{
+		return true;
+	}
+};
+struct _FalseType
+{
+	bool Get()
+	{
+		return false;
+	}
+};
+template<class _Tp>
+struct TypeTraists
+{
+	typedef _FalseType _IsPODType;
+
+	};
+template<>
+struct TypeTraists<bool>
+{
+	typedef _FalseType _IsPODType;
+
+	};
+struct TypeTraists<char>
+{
+	typedef _FalseType _IsPODType;
+
+	};
+struct TypeTraists<unsigned char>
+{
+	typedef _FalseType _IsPODType;
+
+	};
+struct TypeTraists<short>
+{
+	typedef _FalseType _IsPODType;
+
+	};
+struct TypeTraists<unsigned short>
+{
+	typedef _FalseType _IsPODType;
+
+	};
+struct TypeTraists<bool>
+{
+	typedef _FalseType _IsPODType;
+
+	};
+struct TypeTraists<bool>
+{
+	typedef _FalseType _IsPODType;
+
+	};
+struct TypeTraists<bool>
+{
+	typedef _FalseType _IsPODType;
+
+	};
