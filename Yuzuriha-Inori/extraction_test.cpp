294569c6250b126f546f03439057169719fb2d0f
yixi qi <qiyixi2010@Gmail.com>
2017-03-22 21:09:07 +0800

    test extraction

--- Yuzuriha-Inori/extraction_test.cpp
+++ Yuzuriha-Inori/extraction_test.cpp
@@ -26,43 +26,73 @@ struct TypeTraists<bool>
 	typedef _FalseType _IsPODType;
 
 	};
+template<>
 struct TypeTraists<char>
 {
 	typedef _FalseType _IsPODType;
 
 	};
+template<>
 struct TypeTraists<unsigned char>
 {
 	typedef _FalseType _IsPODType;
 
 	};
+template<>
 struct TypeTraists<short>
 {
 	typedef _FalseType _IsPODType;
 
 	};
+template<>
 struct TypeTraists<unsigned short>
 {
 	typedef _FalseType _IsPODType;
 
 	};
+template<>
 struct TypeTraists<int>
 {
 	typedef _FalseType _IsPODType;
 
 	};
+template<>
 struct TypeTraists<unsigned int>
 {
 	typedef _FalseType _IsPODType;
 
 	};
+template<>
 struct TypeTraists<double>
 {
 	typedef _FalseType _IsPODType;
 
 };
-struct TypeTraists<unsigned double>
+template<>
+struct TypeTraists<long>
 {
 	typedef _FalseType _IsPODType;
+
+};
+template<>
+struct TypeTraists<long long>
+{
+	typedef _FalseType _IsPODType;
+
+};
+template<class T>
+void Copy(const T* src,T* dst,size_t size _FALSE Type)
+{
+	cout<<"_FalseType:"<<typeid(T).name()<<endl;
+	for(size_t i=0;i<size;++i)
+	{
+		dst[i]=src[i];
+	}
+}
+template<class T>
+void Copy(const T* src,T* dst,size_t size _FALSE Type)
+{
+	cout<<"_FalseType:"<<typeid(T).name()<<endl;
+	memccpy(dst,src,size*sizeof(T));
 
 }
