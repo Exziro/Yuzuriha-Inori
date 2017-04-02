0041ad5d0930ae112543b0f4612d8fbcd3985975
yixi qi <qiyixi2010@Gmail.com>
2017-04-02 21:14:55 +0800

    one liner

---
+++ Yuzuriha-Inori/growliner.c
@@ -0,0 +1,16 @@
+#include<stdio.h>
+int main()
+{
+	int i=0;
+	char bar[102];
+	bar[0]=0;
+	while(i<=100)
+	{
+		printf("[%s]\r",bar);
+		fflush(stdout);
+		bar[i]='*';
+		i++;
+		bar[i]=0;
+	}
+	return 0;
+}
