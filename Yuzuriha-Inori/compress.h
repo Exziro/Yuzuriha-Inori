6d173fb258f1d6533393477403279b6b69ac697a
yixi qi <qiyixi2010@Gmail.com>
2017-05-14 22:31:37 +0800

    yasuo

---
+++ Yuzuriha-Inori/compress.h
@@ -0,0 +1,31 @@
+#include<iostream>
+using namespace std;
+#include"Btree.hpp"
+struct Infor
+{
+	Infor(int count =0)
+		:_count(count)
+	{
+	}
+	bool operator<(const Infor infor)
+	{
+		return _count<infor._count;
+	}
+	bool operator>(const Infor infor)
+	{
+		return _count>infor._count;
+	}
+		bool operator=(const Infor infor)
+	{
+		return _count=infor._count;
+	}
+		Infor operator+(const Infor infor)
+	{
+		return _count+infor._count;
+	}
+	char ch;
+	int _count;
+	string _code;
+};
+class compress
+{
