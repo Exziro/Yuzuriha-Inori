d295e489331ac21ef201a2a5ae3bfad52254b190
yixi qi <qiyixi2010@Gmail.com>
2017-06-10 18:49:05 +0800

    哈希桶

--- Yuzuriha-Inori/hashingbukect.cpp
+++ Yuzuriha-Inori/hashingbukect.cpp
@@ -64,3 +64,67 @@ private:
 	std::vector<HashElem<K, V>> _table;
 	size_t _size;  // 有效元素的个数
 };
+#pragma once
+#include<iostream>
+#include<string>
+#include<vector>
+
+using namespace std;
+
+template<class T>
+struct DefaultFunc
+{
+	size_t operator()(const T& data)
+	{
+		return (size_t)data;
+	}
+};
+
+struct StringFunc
+{
+	size_t operator()(const string& str)
+	{
+		size_t sum = 0;
+		for (size_t i = 0; i < str.size(); ++i)
+		{
+			sum += str[i];
+		}
+		return sum;
+	}
+};
+
+template<class K, class V>
+struct HashBucketNode
+{
+	K _key;
+	V _value;
+	HashBucketNode<K, V>* _next;
+	HashBucketNode(const K& key, const V& value)
+		:_key(key)
+		, _value(value)
+		, _next(NULL)
+	{}
+};
+
+template<class K, class V, class FuncModle = DefaultFunc<K>>
+class HashBucket
+{
+	typedef HashBucketNode<K, V> Node;
+public:
+	HashBucket();
+	//~HashBucket();
+	HashBucket(const HashBucket<K, V, FuncModle>& h);
+	HashBucket<K, V, FuncModle>& operator=(HashBucket<K, V, FuncModle> h);
+	bool Insert(const K& key, const V& value);
+	Node* Find(const K& key);
+	bool Remove(const K& key);
+	//bool Alter(const K& key);//用Find实现
+	void Print();
+protected:
+	void _CheckExpand();//检查是否需要扩容
+	size_t _GetNextPrime(size_t size);//从素数表中得到比当前素数大的素数
+	size_t _HashFunc(const K& key,size_t mod);//哈希函数
+protected:
+	vector<Node*> _table;
+	size_t _size;//记录的个数
+};
