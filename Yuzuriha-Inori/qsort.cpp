6cb05cfa3d3f91a545de5be90235267059987fe5
yixi qi <qiyixi2010@Gmail.com>
2017-06-26 22:52:24 +0800

    selecte sort

--- Yuzuriha-Inori/qsort.cpp
+++ Yuzuriha-Inori/qsort.cpp
@@ -57,3 +57,34 @@ void SelectionSort(int* pDataArray, int iDataNum)
             DataSwap(&pDataArray[index], &pDataArray[i]);  
     }  
 }  
+using namespace std;
+
+void swap(int &x, int &y)
+{
+    int temp = x;
+    x = y;
+    y = temp;
+}
+
+void insertion(int a[], int sz)
+{
+    for(int i=1; i  < sz; i++) {
+        int j = i;
+        while(j > 0 && (a[j] < a[j-1])) {
+            swap(a[j], a[j-1]);
+            j--;
+        }
+        cout << endl;
+        for (int k = 0; k < sz; k++) cout << setw(3) << a[k];
+    }
+}
+
+int main()
+{
+    int a[] = { 15, 9, 8, 1, 4, 11, 7, 12, 13, 6, 5, 3, 16, 2, 10, 14};
+    int size = sizeof(a)/sizeof(int);
+    for (int i = 0; i < size; i++) cout << setw(3) << a[i];
+    insertion(a, size);
+    cout << endl;
+    return 0;
+}
