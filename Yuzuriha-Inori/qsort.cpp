a10dfc2a042722be7bcf9e035baa4da3ecd505f7
yixi qi <qiyixi2010@Gmail.com>
2017-06-27 22:09:06 +0800

    selecte sort

--- Yuzuriha-Inori/qsort.cpp
+++ Yuzuriha-Inori/qsort.cpp
@@ -88,3 +88,33 @@ int main()
     cout << endl;
     return 0;
 }
+
+void exchange(int A[], int i, int j)        // 交换A[i]和A[j]
+{
+    int temp = A[i];
+    A[i] = A[j];
+    A[j] = temp;
+}
+
+int main()
+{
+    int A[] = { 6, 5, 3, 1, 8, 7, 2, 4 };    // 从小到大冒泡排序
+    int n = sizeof(A) / sizeof(int);                
+    for (int j = 0; j < n - 1; j++)            // 每次最大元素就像气泡一样"浮"到数组的最后
+    {
+        for (int i = 0; i < n - 1 - j; i++)    // 依次比较相邻的两个元素,使较大的那个向后移
+        {
+            if (A[i] > A[i + 1])            // 如果条件改成A[i] >= A[i + 1],则变为不稳定的排序算法
+            {
+                exchange(A, i, i + 1);        
+            }
+        }
+    }
+    printf("冒泡排序结果：");
+    for (int i = 0; i < n; i++)
+    {
+        printf("%d ", A[i]);
+    }
+    printf("\n");
+    return 0;
+}
