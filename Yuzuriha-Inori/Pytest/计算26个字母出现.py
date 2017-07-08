4a30e4ad082a0701c1b2e4d0fa345763ecc00f65
yixi qi <qiyixi2010@Gmail.com>
2017-07-08 23:17:36 +0800

    查找26个字母

--- Yuzuriha-Inori/Pytest/计算26个字母出现.py
+++ Yuzuriha-Inori/Pytest/计算26个字母出现.py
@@ -5,3 +5,9 @@ def letter_frequency(s):
    for i in string.lowercase:
       l.append(s.count(i))            #统计个数
    return l 
+   def letter_frequency(s):
+   s = s.lower()                      #全部转小写
+   l = []
+   for i in [chr(x) for x in range(97,123)]:
+      l.append(s.count(i))            #统计个数
+   return l                           #返回
