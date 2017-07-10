4b62131ebcd97bb651a7f3f3719e1caaf719e06f
yixi qi <qiyixi2010@Gmail.com>
2017-07-11 00:44:23 +0800

    test

---
+++ Yuzuriha-Inori/daliy/merecy.cpp
@@ -0,0 +1,23 @@
+
+ListNode* Merge(ListNode* pHead1, ListNode* pHead2)
+{
+    if(pHead1 == NULL)
+        return pHead2;
+    else if(pHead2 == NULL)
+        return pHead1;
+ 
+    ListNode* pMergedHead = NULL;
+ 
+    if(pHead1->m_nValue < pHead2->m_nValue)
+    {
+        pMergedHead = pHead1;
+        pMergedHead->m_pNext = Merge(pHead1->m_pNext, pHead2);
+    }
+    else
+    {
+        pMergedHead = pHead2;
+        pMergedHead->m_pNext = Merge(pHead1, pHead2->m_pNext);
+    }
+ 
+    return pMergedHead;
+}
