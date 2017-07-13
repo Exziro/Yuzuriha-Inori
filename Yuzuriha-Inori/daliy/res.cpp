79fbeee028ef34b7aef2ee5423b569676369be63
yixi qi <qiyixi2010@Gmail.com>
2017-07-13 22:12:20 +0800

    RES k

--- Yuzuriha-Inori/daliy/res.cpp
+++ Yuzuriha-Inori/daliy/res.cpp
@@ -14,3 +14,33 @@ Node *ReverseList(Node *phead)
     }  
     return r;  
 } 
+node *search(node *head,int k)
+{
+ 
+       int length=0;
+       node *p=head;
+       while (p!=NULL)
+       {
+              length++;
+              p=p->next;
+       }
+       if (list==NULL||k==0||k>length)
+       {
+              return NULL;
+       }
+ 
+       node *p1=head;
+       node *p2=head;
+
+       for (int i=0;i<k-1;i++)
+       {
+              p1=p1->next;
+       }
+
+       while (p1->next!=NULL)
+       {
+              p1=p1->next;
+              p2=p2->next;
+       }
+       return p2;
+}
