579dcf2f17579470296a1479880ba577923ed8a7
yixi qi <qiyixi2010@Gmail.com>
2017-07-14 22:16:01 +0800

    if circle

---
+++ Yuzuriha-Inori/daliy/circle.cpp
@@ -0,0 +1,8 @@
+    typedef int DataType;  
+    typedef struct strNode  
+    {  
+        struct strNode* pNext;  
+        DataType  data;  
+    }Node;  
+      
+    typedef struct strNode*  PNode;  
