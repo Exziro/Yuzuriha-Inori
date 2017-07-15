7e9367507289862f83518cc69a09e45f7e2d1305
yixi qi <qiyixi2010@Gmail.com>
2017-07-15 22:39:48 +0800

    if circle

--- Yuzuriha-Inori/daliy/circle.cpp
+++ Yuzuriha-Inori/daliy/circle.cpp
@@ -1,8 +1,15 @@
-    typedef int DataType;  
-    typedef struct strNode  
-    {  
-        struct strNode* pNext;  
-        DataType  data;  
-    }Node;  
-      
-    typedef struct strNode*  PNode;  
+pair<Node<int>*, bool> IsExitsLoop(Node<int>* head)
+{
+    assert(head);
+    Node<int>* fast = head;
+    Node<int>* slow = head;
+    while (fast&&fast->_next)
+    {
+        fast = fast->_next->_next;
+        slow = slow->_next;
+        if (fast == slow)  //有交点的话 说明有环
+            return make_pair(fast, true);
+    }
+    //到这里说明，不带环，直接返回
+    return make_pair(fast, false);
+}
