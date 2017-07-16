243ccb230be9a2843f0eb7ca00840f59a68c8ed7
yixi qi <qiyixi2010@Gmail.com>
2017-07-16 22:42:02 +0800

    if circle

--- Yuzuriha-Inori/daliy/circle.cpp
+++ Yuzuriha-Inori/daliy/circle.cpp
@@ -13,3 +13,30 @@ pair<Node<int>*, bool> IsExitsLoop(Node<int>* head)
     //到这里说明，不带环，直接返回
     return make_pair(fast, false);
 }
+int length(Node<int>* head)
+{
+    assert(head);
+    if (!IsExitsLoop(head).second)
+        return 0;
+    Node<int>* node=IsExitsLoop(head).first;
+    Node<int>* cur = node->_next;
+    int count = 1;
+    while (cur != node)
+    {
+        count++;
+        cur = cur->_next;
+    }
+    return count;
+}
+Node<int>* FindLoopPort(Node<int>* head)
+{
+    assert(head);
+    Node<int>* node = IsExitsLoop(head).first;
+    Node<int>* cur = head;
+    while (cur != node)
+    {
+        cur = cur->_next;
+        node=node->_next;
+    }
+    return cur;
+}
