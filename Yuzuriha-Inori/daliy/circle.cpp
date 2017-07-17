fb148b7e6ff31e2e4e666549fe889c49c0315c94
yixi qi <qiyixi2010@Gmail.com>
2017-07-17 23:40:24 +0800

    if circle

--- Yuzuriha-Inori/daliy/circle.cpp
+++ Yuzuriha-Inori/daliy/circle.cpp
@@ -1,4 +1,6 @@
-pair<Node<int>*, bool> IsExitsLoop(Node<int>* head)
+#include<iostream>
+pair<Node<int>*, bool> 
+IsExitsLoop(Node<int>* head)
 {
     assert(head);
     Node<int>* fast = head;
@@ -7,10 +9,9 @@ pair<Node<int>*, bool> IsExitsLoop(Node<int>* head)
     {
         fast = fast->_next->_next;
         slow = slow->_next;
-        if (fast == slow)  //有交点的话 说明有环
+        if (fast == slow)
             return make_pair(fast, true);
     }
-    //到这里说明，不带环，直接返回
     return make_pair(fast, false);
 }
 int length(Node<int>* head)
