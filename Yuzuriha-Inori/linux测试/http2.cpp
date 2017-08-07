eef62d89cbc2ad3361026fce428040274e8424da
yixi qi <qiyixi2010@Gmail.com>
2017-08-07 22:40:35 +0800

    http服务器E

--- Yuzuriha-Inori/linux测试/http2.cpp
+++ Yuzuriha-Inori/linux测试/http2.cpp
@@ -29,3 +29,21 @@
 #define ISspace(x) isspace((int)(x))  
   
 #define SERVER_STRING "Server: jdbhttpd/0.1.0\r\n"  
+void accept_request(int);  
+void bad_request(int);  
+void cat(int, FILE *);  
+void cannot_execute(int);  
+void error_die(const char *);  
+void execute_cgi(int, const char *, const char *, const char *);  
+int get_line(int, char *, int);  
+void headers(int, const char *);  
+void not_found(int);  
+void serve_file(int, const char *);  
+int startup(u_short *);  
+void unimplemented(int);  
+  
+/**********************************************************************/  
+/* A request has caused a call to accept() on the server port to 
+ * return.  Process the request appropriately. 
+ * Parameters: the socket connected to the client */  
+/**********************************************************************/ 
