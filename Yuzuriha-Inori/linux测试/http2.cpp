71d34d1b507238689a9fd7fce3de2053ba519da3
yixi qi <qiyixi2010@Gmail.com>
2017-08-11 22:45:02 +0800

    http服务器E

--- Yuzuriha-Inori/linux测试/http2.cpp
+++ Yuzuriha-Inori/linux测试/http2.cpp
@@ -205,3 +205,127 @@ void cannot_execute(int client)
  * on value of errno, which indicates system call errors) and exit the 
  * program indicating an error. */  
 /**********************************************************************/  
+void error_die(const char *sc)  
+{  
+    /*出错信息处理 */  
+    perror(sc);  
+    exit(1);  
+}  
+  
+/**********************************************************************/  
+/* Execute a CGI script.  Will need to set environment variables as 
+ * appropriate. 
+ * Parameters: client socket descriptor 
+ *             path to the CGI script */  
+/**********************************************************************/  
+void execute_cgi(int client, const char *path, const char *method, const char *query_string)  
+{  
+    char buf[1024];  
+    int cgi_output[2];  
+    int cgi_input[2];  
+    pid_t pid;  
+    int status;  
+    int i;  
+    char c;  
+    int numchars = 1;  
+    int content_length = -1;  
+  
+    buf[0] = 'A'; buf[1] = '\0';  
+    if (strcasecmp(method, "GET") == 0)  
+        /*把所有的 HTTP header 读取并丢弃*/  
+        while ((numchars > 0) && strcmp("\n", buf))  /* read & discard headers */  
+            numchars = get_line(client, buf, sizeof(buf));  
+    else    /* POST */  
+    {  
+        /* 对 POST 的 HTTP 请求中找出 content_length */  
+        numchars = get_line(client, buf, sizeof(buf));  
+        while ((numchars > 0) && strcmp("\n", buf))  
+        {  
+            /*利用 \0 进行分隔 */  
+            buf[15] = '\0';  
+            /* HTTP 请求的特点*/  
+            if (strcasecmp(buf, "Content-Length:") == 0)  
+                content_length = atoi(&(buf[16]));  
+            numchars = get_line(client, buf, sizeof(buf));  
+        }  
+        /*没有找到 content_length */  
+        if (content_length == -1) {  
+            /*错误请求*/  
+            bad_request(client);  
+            return;  
+        }  
+    }  
+  
+    /* 正确，HTTP 状态码 200 */  
+    sprintf(buf, "HTTP/1.0 200 OK\r\n");  
+    send(client, buf, strlen(buf), 0);  
+  
+    /* 建立管道*/  
+    if (pipe(cgi_output) < 0) {  
+        /*错误处理*/  
+        cannot_execute(client);  
+        return;  
+    }  
+    /*建立管道*/  
+    if (pipe(cgi_input) < 0) {  
+        /*错误处理*/  
+        cannot_execute(client);  
+        return;  
+    }  
+  
+    if ((pid = fork()) < 0 ) {  
+        /*错误处理*/  
+        cannot_execute(client);  
+        return;  
+    }  
+    if (pid == 0)  /* child: CGI script */  
+    {  
+        char meth_env[255];  
+        char query_env[255];  
+        char length_env[255];  
+  
+        /* 把 STDOUT 重定向到 cgi_output 的写入端 */  
+        dup2(cgi_output[1], 1);  
+        /* 把 STDIN 重定向到 cgi_input 的读取端 */  
+        dup2(cgi_input[0], 0);  
+        /* 关闭 cgi_input 的写入端 和 cgi_output 的读取端 */  
+        close(cgi_output[0]);  
+        close(cgi_input[1]);  
+        /*设置 request_method 的环境变量*/  
+        sprintf(meth_env, "REQUEST_METHOD=%s", method);  
+        putenv(meth_env);  
+        if (strcasecmp(method, "GET") == 0) {  
+            /*设置 query_string 的环境变量*/  
+            sprintf(query_env, "QUERY_STRING=%s", query_string);  
+            putenv(query_env);  
+        }  
+        else {   /* POST */  
+            /*设置 content_length 的环境变量*/  
+            sprintf(length_env, "CONTENT_LENGTH=%d", content_length);  
+            putenv(length_env);  
+        }  
+        /*用 execl 运行 cgi 程序*/  
+        execl(path, path, NULL);  
+        exit(0);  
+    } else {    /* parent */  
+        /* 关闭 cgi_input 的读取端 和 cgi_output 的写入端 */  
+        close(cgi_output[1]);  
+        close(cgi_input[0]);  
+        if (strcasecmp(method, "POST") == 0)  
+            /*接收 POST 过来的数据*/  
+            for (i = 0; i < content_length; i++) {  
+                recv(client, &c, 1, 0);  
+                /*把 POST 数据写入 cgi_input，现在重定向到 STDIN */  
+                write(cgi_input[1], &c, 1);  
+            }  
+        /*读取 cgi_output 的管道输出到客户端，该管道输入是 STDOUT */  
+        while (read(cgi_output[0], &c, 1) > 0)  
+            send(client, &c, 1, 0);  
+  
+        /*关闭管道*/  
+        close(cgi_output[0]);  
+        close(cgi_input[1]);  
+        /*等待子进程*/  
+        waitpid(pid, &status, 0);  
+    }  
+}  
