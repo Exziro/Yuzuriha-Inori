3471eb64785e0cf1c0e5350c669aa40c7475f29a
yixi qi <qiyixi2010@Gmail.com>
2017-08-12 08:26:25 +0800

    http服务器E

--- Yuzuriha-Inori/linux测试/http2.cpp
+++ Yuzuriha-Inori/linux测试/http2.cpp
@@ -329,3 +329,45 @@ void execute_cgi(int client, const char *path, const char *method, const char *q
         waitpid(pid, &status, 0);  
     }  
 }  
+/**********************************************************************/  
+/* This function starts the process of listening for web connections 
+ * on a specified port.  If the port is 0, then dynamically allocate a 
+ * port and modify the original port variable to reflect the actual 
+ * port. 
+ * Parameters: pointer to variable containing the port to connect on 
+ * Returns: the socket */  
+/**********************************************************************/  
+int startup(u_short *port)  
+{  
+    int httpd = 0;  
+    struct sockaddr_in name;  
+  
+    /*建立 socket */  
+    httpd = socket(PF_INET, SOCK_STREAM, 0);  
+    if (httpd == -1)  
+        error_die("socket");  
+    memset(&name, 0, sizeof(name));  
+    name.sin_family = AF_INET;  
+    name.sin_port = htons(*port);  
+    name.sin_addr.s_addr = htonl(INADDR_ANY);  
+    if (bind(httpd, (struct sockaddr *)&name, sizeof(name)) < 0)  
+        error_die("bind");  
+    /*如果当前指定端口是 0，则动态随机分配一个端口*/  
+    if (*port == 0)  /* if dynamically allocating a port */  
+    {  
+        int namelen = sizeof(name);  
+        if (getsockname(httpd, (struct sockaddr *)&name, &namelen) == -1)  
+            error_die("getsockname");  
+        *port = ntohs(name.sin_port);  
+    }  
+    /*开始监听*/  
+    if (listen(httpd, 5) < 0)  
+        error_die("listen");  
+    /*返回 socket id */  
+    return(httpd);  
+}  
+  
+/**********************************************************************/  
+/* Inform the client that the requested web method has not been 
+ * implemented. 
+ * Parameter: the client socket */  
