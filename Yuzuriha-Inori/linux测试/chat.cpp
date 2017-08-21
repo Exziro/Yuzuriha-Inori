0b9d942ded67573cbe23cc53ac106626d3c053d7
yixi qi <qiyixi2010@Gmail.com>
2017-08-21 22:26:40 +0800

    chat example

--- Yuzuriha-Inori/linux测试/chat.cpp
+++ Yuzuriha-Inori/linux测试/chat.cpp
@@ -26,3 +26,28 @@ int main(int argc,char *argv[])
         lisnum=atoi(argv[3]);  
     else  
         lisnum=5; 
+	    if((sockfd=socket(AF_INET,SOCK_STREAM,0))==-1)  
+    {  
+        perror("socket");  
+        exit(EXIT_FAILURE);  
+    }  
+  
+    bzero(&my_addr,sizeof(my_addr));  
+    my_addr.sin_family=AF_INET;  
+    my_addr.sin_port=htons(myport);  
+    if(argv[1])  
+        my_addr.sin_addr.s_addr=inet_addr(argv[1]);  
+    else  
+        my_addr.sin_addr.s_addr=INADDR_ANY;  
+    if(bind(sockfd,(struct sockaddr *)&my_addr,sizeof(struct sockaddr))==-1)  
+    {  
+        perror("bind");  
+        exit(EXIT_FAILURE);  
+    }  
+    if(listen(sockfd,lisnum)==-1)  
+    {  
+        perror("listen");  
+        exit(EXIT_FAILURE);  
+    }  
+    printf("wait for connect!\n");  
+    len=sizeof(struct sockaddr);  
