bfa7acf8037d1e0f55d739a1b714de60c360cf94
yixi qi <qiyixi2010@Gmail.com>
2017-08-27 22:20:09 +0800

    date analysis

---
+++ Yuzuriha-Inori/date＿analysis/try.py
@@ -0,0 +1,13 @@
+# -*- coding: utf-8 -*-
+"""
+Created on Sun Aug 27 22:16:49 2017
+
+@author: yixi
+"""
+
+import pandas as pd #数据分析
+import numpy as np #科学计算
+from pandas import Series,DataFrame
+
+data_train = pd.read_csv("/Users/Hanxiaoyang/Titanic_data/Train.csv")
+data_train
