e33051025adc30744ddc1ce30125abc9ef9efff1
yixi qi <qiyixi2010@Gmail.com>
2017-08-29 22:22:39 +0800

    date analysis

--- Yuzuriha-Inori/date＿analysis/try.py
+++ Yuzuriha-Inori/date＿analysis/try.py
@@ -12,3 +12,11 @@ from pandas import Series,DataFrame
 data_train = pd.read_csv("/Users/Hanxiaoyang/Titanic_data/Train.csv")
 data_train
 data_train.info()
+import matplotlib.pyplot as plt
+fig = plt.figure()
+fig.set(alpha=0.2)  # 设定图表颜色alpha参数
+
+plt.subplot2grid((2,3),(0,0))             # 在一张大图里分列几个小图
+data_train.Survived.value_counts().plot(kind='bar')# 柱状图 
+plt.title(u"获救情况 (1为获救)") # 标题
+plt.ylabel(u"人数")  
