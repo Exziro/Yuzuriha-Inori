10c118ad594fca4533a1adbadd365fdaaedf9f62
yixi qi <qiyixi2010@Gmail.com>
2017-08-30 22:23:20 +0800

    date analysis

--- Yuzuriha-Inori/date＿analysis/try.py
+++ Yuzuriha-Inori/date＿analysis/try.py
@@ -20,3 +20,13 @@ plt.subplot2grid((2,3),(0,0))             # 在一张大图里分列几个小图
 data_train.Survived.value_counts().plot(kind='bar')# 柱状图 
 plt.title(u"获救情况 (1为获救)") # 标题
 plt.ylabel(u"人数")  
+plt.subplot2grid((2,3),(0,1))
+data_train.Pclass.value_counts().plot(kind="bar")
+plt.ylabel(u"人数")
+plt.title(u"乘客等级分布")
+
+plt.subplot2grid((2,3),(0,2))
+plt.scatter(data_train.Survived, data_train.Age)
+plt.ylabel(u"年龄")                         # 设定纵坐标名称
+plt.grid(b=True, which='major', axis='y') 
+plt.title(u"按年龄看获救分布 (1为获救)")
