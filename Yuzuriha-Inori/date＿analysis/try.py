23dac472a191bd6f12f1f260c14dfb221717c143
yixi qi <qiyixi2010@Gmail.com>
2017-08-31 22:23:48 +0800

    date analysis

--- Yuzuriha-Inori/date＿analysis/try.py
+++ Yuzuriha-Inori/date＿analysis/try.py
@@ -30,3 +30,19 @@ plt.scatter(data_train.Survived, data_train.Age)
 plt.ylabel(u"年龄")                         # 设定纵坐标名称
 plt.grid(b=True, which='major', axis='y') 
 plt.title(u"按年龄看获救分布 (1为获救)")
+
+plt.subplot2grid((2,3),(1,0), colspan=2)
+data_train.Age[data_train.Pclass == 1].plot(kind='kde')   
+data_train.Age[data_train.Pclass == 2].plot(kind='kde')
+data_train.Age[data_train.Pclass == 3].plot(kind='kde')
+plt.xlabel(u"年龄")# plots an axis lable
+plt.ylabel(u"密度") 
+plt.title(u"各等级的乘客年龄分布")
+plt.legend((u'头等舱', u'2等舱',u'3等舱'),loc='best') # sets our legend for our graph.
+
+
+plt.subplot2grid((2,3),(1,2))
+data_train.Embarked.value_counts().plot(kind='bar')
+plt.title(u"各登船口岸上船人数")
+plt.ylabel(u"人数")  
+plt.show()
