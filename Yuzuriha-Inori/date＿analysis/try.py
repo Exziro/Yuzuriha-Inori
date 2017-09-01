5c714743f918dc766649fa2b470da3af4fa8e19c
yixi qi <qiyixi2010@Gmail.com>
2017-09-01 22:24:37 +0800

    date analysis

--- Yuzuriha-Inori/date＿analysis/try.py
+++ Yuzuriha-Inori/date＿analysis/try.py
@@ -46,3 +46,15 @@ data_train.Embarked.value_counts().plot(kind='bar')
 plt.title(u"各登船口岸上船人数")
 plt.ylabel(u"人数")  
 plt.show()
+#看看各乘客等级的获救情况
+fig = plt.figure()
+fig.set(alpha=0.2)  # 设定图表颜色alpha参数
+
+Survived_0 = data_train.Pclass[data_train.Survived == 0].value_counts()
+Survived_1 = data_train.Pclass[data_train.Survived == 1].value_counts()
+df=pd.DataFrame({u'获救':Survived_1, u'未获救':Survived_0})
+df.plot(kind='bar', stacked=True)
+plt.title(u"各乘客等级的获救情况")
+plt.xlabel(u"乘客等级") 
+plt.ylabel(u"人数") 
+plt.show()
