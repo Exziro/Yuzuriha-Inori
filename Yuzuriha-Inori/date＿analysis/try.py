0d6f5a2b7a917b268da548ee1a5aba40e6ef156e
yixi qi <qiyixi2010@Gmail.com>
2017-09-02 22:25:17 +0800

    date analysis

--- Yuzuriha-Inori/date＿analysis/try.py
+++ Yuzuriha-Inori/date＿analysis/try.py
@@ -58,3 +58,15 @@ plt.title(u"各乘客等级的获救情况")
 plt.xlabel(u"乘客等级") 
 plt.ylabel(u"人数") 
 plt.show()
+#看看各性别的获救情况
+fig = plt.figure()
+fig.set(alpha=0.2)  # 设定图表颜色alpha参数
+
+Survived_m = data_train.Survived[data_train.Sex == 'male'].value_counts()
+Survived_f = data_train.Survived[data_train.Sex == 'female'].value_counts()
+df=pd.DataFrame({u'男性':Survived_m, u'女性':Survived_f})
+df.plot(kind='bar', stacked=True)
+plt.title(u"按性别看获救情况")
+plt.xlabel(u"性别") 
+plt.ylabel(u"人数")
+plt.show()
