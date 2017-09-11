9d778932e78dd07ea705683cf2064ba959dffa6b
yixi qi <qiyixi2010@Gmail.com>
2017-09-11 22:12:45 +0800

    titanic4

---
+++ Yuzuriha-Inori/date＿analysis/lool.py
@@ -0,0 +1,166 @@
+# -*- coding: utf-8 -*-
+"""
+Created on Thu Sep 14 21:04:27 2017
+
+@author: yixi
+"""
+import sys
+reload(sys)
+sys.setdefaultencoding('utf8')
+import matplotlib
+matplotlib.use('qt4agg')
+#指定默认字体
+matplotlib.rcParams['font.sans-serif'] = ['SimHei']
+matplotlib.rcParams['font.family']='sans-serif'
+import pandas as pd
+import numpy as np
+#from pandas import Series,DataFrame
+data_train = pd.read_csv("C:\Users\yixi\AppData\Local\Temp\Train.csv")
+import matplotlib.pyplot as plt
+'''
+fig = plt.figure()
+fig.set(alpha=0.2)  # 设定图表颜色alpha参数
+
+plt.subplot2grid((2,3),(0,0))             # 在一张大图里分列几个小图
+data_train.Survived.value_counts().plot(kind='bar')# plots a bar graph of those who surived vs those who did not. 
+plt.title(u"获救情况 (1为获救)") # puts a title on our graph
+plt.ylabel(u"人数")  
+
+plt.subplot2grid((2,3),(0,1))
+data_train.Pclass.value_counts().plot(kind="bar")
+plt.ylabel(u"人数")
+plt.title(u"乘客等级分布")
+
+plt.subplot2grid((2,3),(0,2))
+plt.scatter(data_train.Survived, data_train.Age)
+plt.ylabel(u"年龄")                         # sets the y axis lable
+plt.grid(b=True, which='major', axis='y') # formats the grid line style of our graphs
+plt.title(u"按年龄看获救分布 (1为获救)")
+
+
+plt.subplot2grid((2,3),(1,0), colspan=2)
+data_train.Age[data_train.Pclass == 1].plot(kind='kde')   # plots a kernel desnsity estimate of the subset of the 1st class passanges's age
+data_train.Age[data_train.Pclass == 2].plot(kind='kde')
+data_train.Age[data_train.Pclass == 3].plot(kind='kde')
+plt.xlabel(u"年龄")# plots an axis lable
+plt.ylabel(u"密度") 
+plt.title(u"各等级的乘客年龄分布")
+plt.legend((u'头等舱', u'2等舱',u'3等舱'),loc='best') # sets our legend for our graph.
+
+
+plt.subplot2grid((2,3),(1,2))
+data_train.Embarked.value_counts().plot(kind='bar')
+plt.title(u"各登船口岸上船人数")
+plt.ylabel(u"人数") 
+'''
+
+ # 设定图表颜色alpha参数
+Survived_m = data_train.Survived[data_train.Sex == 'male'].value_counts()
+Survived_f = data_train.Survived[data_train.Sex == 'female'].value_counts()
+df=pd.DataFrame({u'男性':Survived_m, u'女性':Survived_f})
+df.plot(kind='bar', stacked=True)
+plt.title(u"性别获救情况")
+plt.xlabel(u"性别") 
+plt.ylabel(u"人数")
+plt.show()
+fig=plt.figure()
+fig.set(alpha=0.65) # 设置图像透明度，无所谓
+plt.title(u"根据舱等级和性别的获救情况")
+
+ax1=fig.add_subplot(141)
+data_train.Survived[data_train.Sex == 'female'][data_train.Pclass != 3].value_counts().plot(kind='bar', label="female highclass", color='#FA2445')
+ax1.set_xticklabels([u"获救", u"未获救"], rotation=0)
+ax1.legend([u"女性/高级舱"], loc='best')
+
+ax2=fig.add_subplot(142, sharey=ax1)
+data_train.Survived[data_train.Sex == 'female'][data_train.Pclass == 3].value_counts().plot(kind='bar', label='female, low class', color='black')
+ax2.set_xticklabels([u"未获救", u"获救"], rotation=0)
+plt.legend([u"女性/低级舱"], loc='best')
+
+ax3=fig.add_subplot(143, sharey=ax1)
+data_train.Survived[data_train.Sex == 'male'][data_train.Pclass != 3].value_counts().plot(kind='bar', label='male, high class',color='blue')
+ax3.set_xticklabels([u"未获救", u"获救"], rotation=0)
+plt.legend([u"男性/高级舱"], loc='best')
+
+ax4=fig.add_subplot(144, sharey=ax1)
+data_train.Survived[data_train.Sex == 'male'][data_train.Pclass == 3].value_counts().plot(kind='bar', label='male low class', color='yellow')
+ax4.set_xticklabels([u"未获救", u"获救"], rotation=0)
+plt.legend([u"男性/低级舱"], loc='best')
+
+plt.show()
+'''g = data_train.groupby(['SibSp','Survived'])
+d1 = pd.DataFrame(g.count()['PassengerId'])'''
+P = data_train.groupby(['Parch','Survived'])
+d2 = pd.DataFrame(P.count()['PassengerId'])
+fig = plt.figure()
+fig.set(alpha=0.2)  # 设定图表颜色alpha参数
+
+Survived_cabin = data_train.Survived[pd.notnull(data_train.Cabin)].value_counts()
+Survived_nocabin = data_train.Survived[pd.isnull(data_train.Cabin)].value_counts()
+df=pd.DataFrame({u'Y':Survived_cabin, u'N':Survived_nocabin}).transpose()
+df.plot(kind='bar', stacked=True)
+plt.title(u"按Cabin有无看获救情况")
+plt.xlabel(u"Cabin有无") 
+plt.ylabel(u"人数")
+plt.show()
+from sklearn.ensemble import RandomForestRegressor
+ 
+### 使用 RandomForestClassifier 填补缺失的年龄属性
+def set_missing_ages(df):
+    
+    # 把已有的数值型特征取出来丢进Random Forest Regressor中
+    age_df = df[['Age','Fare', 'Parch', 'SibSp', 'Pclass']]
+
+    # 乘客分成已知年龄和未知年龄两部分
+    known_age = age_df[age_df.Age.notnull()].as_matrix()
+    unknown_age = age_df[age_df.Age.isnull()].as_matrix()
+
+    # y即目标年龄
+    y = known_age[:, 0]
+
+    # X即特征属性值
+    X = known_age[:, 1:]
+
+    # fit到RandomForestRegressor之中
+    rfr = RandomForestRegressor(random_state=0, n_estimators=2000, n_jobs=-1)
+    rfr.fit(X, y)
+    
+    # 用得到的模型进行未知年龄结果预测
+    predictedAges = rfr.predict(unknown_age[:, 1::])
+    
+    # 用得到的预测结果填补原缺失数据
+    df.loc[ (df.Age.isnull()), 'Age' ] = predictedAges 
+    
+    return df, rfr
+
+def set_Cabin_type(df):
+    df.loc[ (df.Cabin.notnull()), 'Cabin' ] = "Yes"
+    df.loc[ (df.Cabin.isnull()), 'Cabin' ] = "No"
+    return df
+
+data_train, rfr = set_missing_ages(data_train)
+data_train = set_Cabin_type(data_train)
+dummies_Cabin = pd.get_dummies(data_train['Cabin'], prefix= 'Cabin')
+
+dummies_Embarked = pd.get_dummies(data_train['Embarked'], prefix= 'Embarked')
+
+dummies_Sex = pd.get_dummies(data_train['Sex'], prefix= 'Sex')
+
+dummies_Pclass = pd.get_dummies(data_train['Pclass'], prefix= 'Pclass')
+
+df = pd.concat([data_train, dummies_Cabin, dummies_Embarked, dummies_Sex, dummies_Pclass], axis=1)
+df.drop(['Pclass', 'Name', 'Sex', 'Ticket', 'Cabin', 'Embarked'], axis=1, inplace=True)
+from sklearn import linear_model
+
+train_df = df.filter(regex='Survived|Age_.*|SibSp|Parch|Fare_.*|Cabin_.*|Embarked_.*|Sex_.*|Pclass_.*')
+train_np = train_df.as_matrix()
+
+# y即Survival结果
+y = train_np[:, 0]
+
+# X即特征属性值
+X = train_np[:, 1:]
+
+# fit到RandomForestRegressor之中
+clf = linear_model.LogisticRegression(C=1.0, penalty='l1', tol=1e-6)
+clf.fit(X, y)
